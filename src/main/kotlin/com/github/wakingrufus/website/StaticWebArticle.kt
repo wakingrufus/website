027bb2d7b9a07bec7245dd138200fb011c6a5c62
John Burns <wakingrufus@gmail.com>
2018-11-15 08:21:54 -0600

    Draft of DSL article with feedback

--- src/main/kotlin/com/github/wakingrufus/website/StaticWebArticle.kt
+++ src/main/kotlin/com/github/wakingrufus/website/StaticWebArticle.kt
@@ -28,34 +28,42 @@ fun staticweb(): HTML.() -> Unit = {
 
 fun introToStaticWeb(): DIV.() -> Unit = {
     p {
-        +"""
-            The term "static web" refers to a style of web development that might seem out of place in today's
-            modern scene of Javascript frameworks. But it is precisely the proliferation of javascript-based
-            web applications that inspires a reaction to get back to some of the ideals of the web of yesteryear.
-            In this article, we will review the benefits of the static web and look at how Kotlin can bring us
-            some of the benefits of dynamic web programming to the static web world.
-                """.trimIndent()
+        +"""The term "static web" refers to a style of web development that might seem out of place in today's
+            |modern scene of Javascript frameworks.
+            |Not only are social media (Facebook, Twitter, etc) and email (GMail, Yahoo, etc) applications delivered as
+            |Javascript-based "Single Page Applications", but also document-based websites, such as news websites and blogs.
+            |However, it is this proliferation of javascript-based
+            |web applications that inspires a reaction to get back to some of the ideals of the web of yesteryear.
+            |In this article, we will review the benefits of the static web and look at how Kotlin can bring us
+            |some of the benefits of dynamic web programming to the static web world.
+            """.trimMargin()
     }
     h2 { +"Documents, not Apps" }
     p {
         +"""
-            The Web was designed to provide documents, in HTML.
-            The addition of Javascript and AJAX to the standard set of web technologies allowed these documents to be dynamic.
-            This opened up the door to a new way to deliver cross-platform applications with rich UIs.
+            The Web was designed to provide documents.
+            HTML is a markup language based on XML which was designed to be the standard format for documents on the web.
+            The addition of Javascript (JS) and AJAX to the standard set of web technologies allowed these documents to be dynamic.
+            Javascript enables programming logic to run within the browser on a web page.
+            AJAX allows JS to make requests to servers to send or receive additional information, within a single web page.
+            This opened up a doorway to deliver cross-platform applications with rich user interfaces.
             The JS ecosystem developed around this idea of "Web Applications" in order to make it easier to develop these webapps
             and bring along the perks of a true development environment. Pretty soon, this JS ecosystem
             became so ubiquitous, that nearly all websites on the internet are web applications.
             """.trimIndent()
     }
     p {
-        +"""
-            But this has had some downsides. The amount of data transferred when loading a website has exploded.
-            Sites are built on JS libraries which are built on other libraries which are built on others...
-            Many memes have been written about the `node_modules` directory.
-            All of those files are compiled into the application that is delivered as part of a page load.
-            On top of this, sites have stuffed a myriad of trackers and advertisements into their sites
-            in order to monetize you via surveillance capitalism.
-            In addition to the privacy issues with this, it also makes these sites near unusable on slow connections
+        +"""But this has had some downsides. The amount of data transferred when loading a website has exploded.
+            |Sites are built on JS libraries which are built on other libraries which are built on others...
+            |Many memes have been written about the `node_modules` directory.
+            |All of those files are compiled into the application that is delivered as part of a page load.
+            |On top of this, sites have stuffed a myriad of trackers and advertisements into their sites
+            |in order to monetize you via
+            """.trimMargin()
+        +" "
+        a(href="https://en.wikipedia.org/wiki/Surveillance_capitalism"){+"surveillance capitalism"}
+        +". "
+        +"""In addition to the privacy issues with this, it also makes these sites near unusable on slow connections
             such as mobile connections or connections in rural areas or developing countries.
             """.trimIndent()
     }
@@ -68,21 +76,23 @@ fun introToStaticWeb(): DIV.() -> Unit = {
             There is a new static webhost called
             """.trimIndent()
         +" "
-        a(href = "http://www.neocites.org") { +"Neocities" }
+        a(href = "http://neocites.org") { +"Neocities" }
         +" a name which is an homage to the defunct Geocities, where many people are building static sites."
     }
     p {
         +"""
-            I decided to build myself a personal website to publish my own content to, in order to avoid places like Medium, Twitter, or various Slideshow services.
+            I decided to build myself a personal website in order to avoid places like Medium, Twitter, or various slideshow services.
             Anyone who has built a site by hand in HTML knows that this is very tedious.
             There are some frameworks for generating static sites, such as Jekyll and Hugo, which are great for most people.
-            But I am a programmer, and i'd like the control i get from programming, with the benefits of using a framework as well.
+            But I am a programmer, and I'd like the control I get from programming, with the benefits of using a framework as well.
+            In this example, instead of using a template engine such as Jekyll or Hugo, I will be using a "Domain-Specific Language", or DSL.
+            A DSL is a simplified programming language designed for a specific use case.
             This is where Kotlin comes in.
             """.trimIndent()
     }
     p {
-        +"""
-            Kotlin is a very good language for DSLs. THe Kotlin team has created HTML and CSS DSLs already.
+        +"""Kotlin's functional programming capabilities allows it to be used to create DSLs.
+            The Kotlin team has created HTML and CSS DSLs already.
             Instead of building a DSL from scratch, I decided to extend these DSLs. I will walk you thought how I did this.
             Along the way, we will see how DSLs are built, using the HTML DSL as a model example,
             then how to write our own DSLs by extending the HTML DSL.
