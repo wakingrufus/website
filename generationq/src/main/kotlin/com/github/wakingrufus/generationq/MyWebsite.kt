227c87e597d897cf2a16be00c2ea1f67bf04d883
wakingrufus <wakingrufus@gmail.com>
2020-07-28 16:50:10 -0500

    add episode 1 of generationQ

--- generationq/src/main/kotlin/com/github/wakingrufus/generationq/MyWebsite.kt
+++ generationq/src/main/kotlin/com/github/wakingrufus/generationq/MyWebsite.kt
@@ -9,8 +9,6 @@ import java.io.File
 
 object Paths {
     val CSS_PATH = "styles.css"
-    val RSS_PATH = "rss.xml"
-    val SITE_UPDATES_RSS_PATH = "site-updates.xml"
 }
 
 val myFooter: FOOTER.() -> Unit = {
@@ -26,7 +24,7 @@ fun HtmlPage.standardHead() {
     this.head {
         style(type = "text/css") { unsafe { raw(MyStyles().globalStyles()) } }
         link(href = Paths.CSS_PATH, rel = "stylesheet")
-        link(href = "https://generationq.neocities.org/rss/" + Paths.RSS_PATH, type = "application/rss+xml", rel = "alternate") {
+        link(href = "http://generationq.libsyn.com/rss", type = "application/rss+xml", rel = "alternate") {
             title = "RSS"
         }
         title(getTitle())
@@ -67,44 +65,101 @@ val mainPage = htmlPage("index.html") {
                     }
                 }
             }
+
+            div {
+                style = css {
+                    paddingLeft = 1.em
+                }
+                h1 { +"Latest Episode" }
+                episode1()
+            }
+
             div {
                 style = css {
                     paddingLeft = 1.em
                 }
                 h1 { +"Episodes" }
+                span { a(href = "#s1") { +"Season 1" } }
+                episode0()
 
                 div {
                     style = css {
-                        backgroundColor = MyStyles.LIGHT_BACKGROUND_COLOR
+                        paddingTop = 1.em
+                        paddingRight = 1.em
                         paddingBottom = 1.em
-                        borderColor = Color.white
+                        paddingLeft = 1.em
                         borderWidth = 2.px
-
+                        borderColor = Color.silver
                     }
-                    h2 { +"000 - Intro" }
-                    p { +"The hosts John and Andy introduce themselves, and what their goal is with this podcast." }
-                    p {
-                        a(href = "https://archive.org/details/GenerationQ-000-Intro") { +"MP3" }
-                    }
-                    p {
-                        a(href = "https://tenforward.social/@TanagraTooter/104560793009745258") { +"Discuss this episode" }
-                    }
-                    p {
-                        a(href = "https://www.deviantart.com/benttibisson/art/Star-Trek-the-Next-Generation-painting-617241159") { +"Artwork: Bentti Bisson" }
-                    }
-                    p {
-                        a(href = "https://soundcloud.com/nes-house") {
-                            +"Intro Music: NES House - Star Trek the next generation House"
-                        }
-                    }
-                    p {
-                        a(href = "https://soundcloud.com/aeop") {
-                            +"Outro Music: Aeop - A308 - What Do You Want Will Riker"
-                        }
-                    }
+                    a { id = "s1" }
+                    h2 { +"Season 1" }
+                    episode1()
                 }
             }
         }
         footer { myFooter() }
+    }
+}
+
+val episode0: DIV.() -> Unit = {
+    episode("000 - Intro", "https://archive.org/details/GenerationQ-000-Intro",
+            "https://tenforward.social/@TanagraTooter/104560793009745258") {
+        p { +"The hosts John and Andy introduce themselves, and what their goal is with this podcast." }
+    }
+}
+
+val episode1: DIV.() -> Unit = {
+    episode("001 - S1E01 - Encounter at Farpoint",
+            "https://archive.org/details/001-s1e01-encounter-at-farpoint",
+            "https://tenforward.social/@TanagraTooter/104593651455494052") {
+        p {
+            +"""We look at the pilot episode "Encounter at Farpoint", 
+                |a 2-part episode that introduces us to the Next Generation crew and establishes some of the main themes of this series.
+            """.trimMargin()
+        }
+        p { +"This episode is longer due to covering both parts of the episode (technically episodes 1&2)" }
+    }
+}
+
+fun DIV.episode(title: String, url: String, discussionUrl: String, description: DIV.() -> Unit) {
+    div {
+        style = css {
+            backgroundColor = MyStyles.LIGHT_BACKGROUND_COLOR
+            paddingBottom = 1.em
+            borderColor = Color.white
+            borderWidth = 2.px
+
+        }
+        h2 { +title }
+        description()
+        p {
+            a(href = url) { +"MP3" }
+        }
+        p {
+            a(href = discussionUrl) { +"Discuss this episode" }
+        }
+        p {
+            span {
+                a(href = "https://www.deviantart.com/benttibisson/art/Star-Trek-the-Next-Generation-painting-617241159") {
+                    +"Artwork: Bentti Bisson"
+                }
+            }
+            span {
+                +" - "
+            }
+            span {
+                a(href = "https://soundcloud.com/nes-house") {
+                    +"Intro Music: NES House - Star Trek the next generation House"
+                }
+            }
+            span {
+                +" - "
+            }
+            span {
+                a(href = "https://soundcloud.com/aeop") {
+                    +"Outro Music: Aeop - A308 - What Do You Want Will Riker"
+                }
+            }
+        }
     }
 }
