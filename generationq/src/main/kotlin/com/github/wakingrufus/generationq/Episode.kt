0503a4e4b0810ee911704e4da4bd9f404ad5b016
wakingrufus <wakingrufus@gmail.com>
2020-12-01 21:48:58 -0600

    add generation Q ep 10
    add episode images to generation q

--- generationq/src/main/kotlin/com/github/wakingrufus/generationq/Episode.kt
+++ generationq/src/main/kotlin/com/github/wakingrufus/generationq/Episode.kt
@@ -1,55 +1,92 @@
 package com.github.wakingrufus.generationq
 
-import kotlinx.css.Color
-import kotlinx.css.em
-import kotlinx.css.px
+import kotlinx.css.*
 import kotlinx.html.*
+import java.nio.charset.Charset
+import java.util.*
+
 
 class Episode(val title: String, val url: String) {
     var discussionUrl: String? = null
+    var imageName: String? = null
     private var description: DIV.() -> Unit = {}
     fun description(builder: DIV.() -> Unit) {
         this.description = builder
     }
 
-    operator fun invoke(div: DIV){
+    operator fun invoke(div: DIV) {
         div.apply {
             div {
                 style = css {
                     backgroundColor = MyStyles.LIGHT_BACKGROUND_COLOR
-                    paddingBottom = 1.em
+                    paddingTop = 0.5.em
+                    paddingLeft = 0.5.em
+                    paddingBottom = 0.5.em
+                    paddingRight = 0.5.em
                     borderColor = Color.white
                     borderWidth = 2.px
-
+                    borderStyle = BorderStyle.solid
+                    display = Display.flex
+                    flexDirection = FlexDirection.row
                 }
-                h2 { +this@Episode.title }
-                description()
-                p {
-                    a(href = url) { +"MP3" }
+                div {
+                    style = css {
+                        alignSelf = Align.center
+                        display = Display.inlineBlock
+                        flexBasis = FlexBasis("200px")
+                    }
+                    imageName
+                            ?.let { this::class.java.classLoader.getResource(it) }
+                            ?.also {
+                                img(src = "data:image/png;base64,"
+                                        + String(Base64.getEncoder().encode(it.readBytes()), Charset.forName("UTF-8"))) {
+                                    style = css {
+                                        display = Display.inlineBlock
+                                        maxWidth = 200.px
+                                    }
+                                }
+                            }
                 }
-                p {
-                    a(href = discussionUrl) { +"Discuss this episode" }
-                }
-                p {
-                    span {
-                        a(href = "https://www.deviantart.com/benttibisson/art/Star-Trek-the-Next-Generation-painting-617241159") {
-                            +"Artwork: Bentti Bisson"
+                div {
+                    style = css {
+                        display = Display.block
+                        marginLeft = 0.5.em
+                        paddingLeft = 0.5.em
+                    }
+                    h2 {
+                        style = css {
+                            marginTop = 0.px
                         }
+                        +this@Episode.title
                     }
-                    span {
-                        +" - "
+                    description()
+                    p {
+                        a(href = url) { +"MP3" }
                     }
-                    span {
-                        a(href = "https://soundcloud.com/nes-house") {
-                            +"Intro Music: NES House - Star Trek the next generation House"
+                    p {
+                        a(href = discussionUrl) { +"Discuss this episode" }
+                    }
+                    p {
+                        span {
+                            a(href = "https://www.deviantart.com/benttibisson/art/Star-Trek-the-Next-Generation-painting-617241159") {
+                                +"Artwork: Bentti Bisson"
+                            }
                         }
-                    }
-                    span {
-                        +" - "
-                    }
-                    span {
-                        a(href = "https://soundcloud.com/aeop") {
-                            +"Outro Music: Aeop - A308 - What Do You Want Will Riker"
+                        span {
+                            +" - "
+                        }
+                        span {
+                            a(href = "https://soundcloud.com/nes-house") {
+                                +"Intro Music: NES House - Star Trek the next generation House"
+                            }
+                        }
+                        span {
+                            +" - "
+                        }
+                        span {
+                            a(href = "https://soundcloud.com/aeop") {
+                                +"Outro Music: Aeop - A308 - What Do You Want Will Riker"
+                            }
                         }
                     }
                 }
--- generationq/src/main/kotlin/com/github/wakingrufus/generationq/GenerationQ.kt
+++ generationq/src/main/kotlin/com/github/wakingrufus/generationq/GenerationQ.kt
@@ -28,6 +28,7 @@ object GenerationQ : Podcast() {
         val episode3 = episode("003 - S1E03 - The Naked Now",
                 "https://archive.org/details/002-s1e03-the-naked-now") {
             discussionUrl = "https://tenforward.social/@TanagraTooter/104633016860860724"
+            imageName = "ep2.jpg"
             description {
                 p {
                     +"We watch “The Naked Now”, quite possibly the worst episode of the series, but hey, sex sells."
@@ -51,6 +52,7 @@ object GenerationQ : Podcast() {
         val episode5 = episode("005 - S1E05 - The Last Outpost",
                 "https://archive.org/details/GenerationQ-005-S1E5-TheLastOutpost") {
             discussionUrl = "https://tenforward.social/@TanagraTooter/104689478681842924"
+            imageName = "ep5.png"
             description {
                 p {
                     +"We review The Last Outpost, our first look at the Ferengi."
@@ -61,6 +63,7 @@ object GenerationQ : Podcast() {
         val episode6 = episode("006 - S1E06 - Where No One Has Gone Before",
                 "https://archive.org/details/generationq-s1e6") {
             discussionUrl = "https://tenforward.social/@TanagraTooter/104830242366600858"
+            imageName = "ep6.png"
             description {
                 p { +"We review Where No One Has Gone Before and take a trip." }
             }
@@ -70,6 +73,7 @@ object GenerationQ : Podcast() {
                 "https://archive.org/details/GenerationQ-S1E07-LonelyAmongUs"
         ) {
             discussionUrl = "https://tenforward.social/@TanagraTooter/104880119881367074"
+            imageName = "ep7.png"
             description {
                 p { +"We review Lonely Among Us, in which we upload ourselves to the cloud." }
             }
@@ -78,6 +82,7 @@ object GenerationQ : Podcast() {
         episode("008 - S1E08 - Justice",
                 "https://archive.org/details/generationq-008") {
             discussionUrl = "https://tenforward.social/@TanagraTooter/105036382906419475"
+            imageName = "ep8.png"
             description {
                 p { +"You know, I came to this sex planet to get away from things like lectures about justice." }
             }
@@ -86,8 +91,24 @@ object GenerationQ : Podcast() {
         episode("009 - S1E09 - The Battle",
                 "https://archive.org/details/generation-q-009") {
             discussionUrl = "https://tenforward.social/@TanagraTooter/105084689212363611"
+            imageName = "ep9.png"
             description {
                 p { +"I didn't expect to be explaining hashing algorithms on this podcast, but here we are." }
+            }
+        }
+
+        episode("010 - S1E10 - Hide and Q",
+                "https://archive.org/details/generationq-010") {
+            discussionUrl = "https://tenforward.social/@TanagraTooter/105266789173514217"
+            imageName = "ep10.png"
+            description {
+                p {
+                    +"""I apologize for the inconsistency we have been getting these out, but editing can be time consuming,
+                        |and life gets in the way. 
+                        |In this episode, we review S1E10 "Hide and Q".  
+                        |We talk about Q and Picard's ongoing bet, and how it relates to politics in our time."""
+                            .trimMargin()
+                }
             }
         }
     }
